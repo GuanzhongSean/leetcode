19c95357673a2c3ea5775b8afad4931505578ab8
GuanzhongSean <guanzhongsean@outlook.com>
2023-12-17 15:54:47 +0800

    upload q20

---
+++ q20/solution.cc
@@ -0,0 +1,33 @@
+#include <stack>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+      if (s.size() % 2) return false;
+      stack<char> container;
+      for (char ch : s) {
+        if (container.empty()) {
+          container.push(ch);
+          continue;
+        } else {
+          if (ch - container.top() == 1 || ch - container.top() == 2) {
+            container.pop();
+          } else {
+            container.push(ch);
+          }
+        }
+      }
+      return container.empty();
+    }
+};
+
+int main() {
+    Solution s;
+    auto result = s.isValid("([)][]{}");
+    cout << result << endl;
+}
