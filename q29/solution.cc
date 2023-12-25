d3cc47f3a8ca1b5dc9c29d65e92c87c364ea4409
GuanzhongSean <guanzhongsean@outlook.com>
2023-12-25 21:08:46 +0800

    upload q29

---
+++ q29/solution.cc
@@ -0,0 +1,30 @@
+#include <climits>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        // -2^{31} / -1 = 2^31 will overflow, so return 2^31 - 1.
+        if (dividend == INT_MIN && divisor == -1)
+            return INT_MAX;
+        const int sign = dividend > 0 ^ divisor > 0 ? -1 : 1;
+        long ans = 0;
+        long dvd = labs(dividend);
+        long dvs = labs(divisor);
+        while (dvd >= dvs) {
+            long k = 1;
+            while (k * 2 * dvs <= dvd)
+                k *= 2;
+            dvd -= k * dvs;
+            ans += k;
+        }
+        return sign * ans;
+    }
+};
+
+int main() {
+    Solution s;
+    cout << s.divide(INT_MIN, 2) << endl;
+}
