abe88ae7b8f3b094064aa98dce58823d191c5c6e
GuanzhongSean <guanzhongsean@outlook.com>
2023-12-30 16:59:13 +0800

    upload q35

---
+++ q35/solution.cc
@@ -0,0 +1,32 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        if (r == -1 || nums[l] >= target)
+            return 0;
+        else if (nums[r] < target)
+            return r + 1;
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (nums[m] == target)
+                return m;
+            else if (nums[m] > target)
+                r = m - 1;
+            else
+                l = m + 1;
+        }
+        return l;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> nums{5,7,7,8,8, 8, 10};
+    auto result =  s.searchInsert(nums, 6);
+    cout << result << endl;
+}
