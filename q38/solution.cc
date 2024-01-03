48bd47527fa42a17a48a3d2f6fadfc662566f406
GuanzhongSean <guanzhongsean@outlook.com>
2024-01-03 12:53:31 +0800

    upload q38

---
+++ q38/solution.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n==1) return "1";
+        string str = countAndSay(n-1);
+        string tmp{};
+        char c = str[0];
+        int count = 0;
+        for (char ch : str) {
+            if (ch == c) {
+                count++;
+            } else {
+                tmp += '0' + count;
+                tmp += c;
+                c = ch;
+                count = 1;
+            }
+        }
+        tmp += '0' + count;
+        tmp += c;
+        return tmp;
+    }
+};
+
+int main() {
+    Solution s;
+    auto result =  s.countAndSay(4);
+    cout << result << endl;
+}
