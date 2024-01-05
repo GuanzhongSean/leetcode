fafe18d547f270456fe7edd0deb077e89b66eaae
GuanzhongSean <guanzhongsean@outlook.com>
2024-01-05 14:42:41 +0800

    update q41

--- q41/solution.cc
+++ q41/solution.cc
@@ -33,7 +33,7 @@ public:
             if (abs(nums[i]) <= n && nums[abs(nums[i]) - 1] > 0)
                 nums[abs(nums[i]) - 1] *= -1;
         }
-        print(nums)
+        print(nums);
         for (int i = 0; i < n; i++) {
             if (nums[i] > 0)
                 return i + 1;
