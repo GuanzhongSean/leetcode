c2280ef0b4a93ab91d6eedf1ce00bfc24bc3497f
Jiaze Xiao <sean.xiao@uwaterloo.ca>
2024-04-15 13:29:13 -0400

    upload q330

---
+++ q330/solution.cc
@@ -0,0 +1,22 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int minPatches(vector<int> &nums, int n) {
+    long maxReach = 0;
+    int count = 0, i = 0;
+    while (maxReach < n) {
+      if (i < nums.size() && nums[i] <= maxReach + 1) {
+        maxReach += nums[i++];
+      } else {
+        maxReach += maxReach + 1;
+        count++;
+      }
+    }
+    return count;
+  }
+};
+
+int main() {}
