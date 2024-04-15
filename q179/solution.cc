c8cbd8eb658da224ac056dc50233db016b69aa67
Jiaze Xiao <sean.xiao@uwaterloo.ca>
2024-04-15 12:23:01 -0400

    upload q179

---
+++ q179/solution.cc
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  string largestNumber(vector<int> &arr) {
+    vector<string> s;
+    for (int i : arr) {
+      s.push_back(to_string(i));
+    }
+    sort(s.begin(), s.end(),
+         [&](string a, string b) { return (a + b > b + a); });
+    string ans = "";
+    for (int i = 0; i < arr.size(); i++)
+      ans += s[i];
+    if (ans[0] == '0')
+      return "0";
+    return ans;
+  }
+};
+
+int main() {}
