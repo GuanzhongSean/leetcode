5adc32d799a00c804e9bdf9bd5f22e81644997d4
Jiaze Xiao <sean.xiao@uwaterloo.ca>
2024-04-26 20:07:49 -0400

    upload q56

---
+++ q56/solution.cc
@@ -0,0 +1,25 @@
+#include "../include/utils.h"
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+  vector<vector<int>> merge(vector<vector<int>> &intervals) {
+    int n = intervals.size();
+    sort(intervals.begin(), intervals.end());
+    vector<vector<int>> result;
+    result.push_back(intervals[0]);
+    for (int i = 1; i < n; i++)
+      if (intervals[i][0] <= result.back()[1])
+        result.back()[1] = max(result.back()[1], intervals[i][1]);
+      else
+        result.push_back(intervals[i]);
+    return result;
+  }
+};
+
+int main() {
+  vector<vector<int>> intervals{{1, 4}, {1, 5}};
+  cout << Solution().merge(intervals);
+}
