d423f016781222fe57a56bf86354bd0f8619e0b7
Jiaze Xiao <sean.xiao@uwaterloo.ca>
2024-04-28 19:09:30 -0400

    upload q66

---
+++ q66/solution.cc
@@ -0,0 +1,26 @@
+#include "../include/utils.h"
+
+using namespace std;
+
+class Solution {
+public:
+  vector<int> plusOne(vector<int> &digits) {
+    int i = digits.size() - 1;
+    vector<int> ans{digits};
+    ans[i]++;
+    while (ans[i] == 10) {
+      ans[i--] = 0;
+      if (i < 0) {
+        ans.insert(ans.begin(), 1);
+        break;
+      }
+      ans[i]++;
+    }
+    return ans;
+  }
+};
+
+int main() {
+  vector<int> v{9, 9, 9};
+  cout << Solution().plusOne(v);
+}
