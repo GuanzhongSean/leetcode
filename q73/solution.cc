fcb6b3e29e8924049c2ad232da1c209d92aed8ee
Jiaze Xiao <sean.xiao@uwaterloo.ca>
2024-05-02 15:27:13 -0400

    upload q73

---
+++ q73/solution.cc
@@ -0,0 +1,28 @@
+#include "../include/utils.h"
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  void setZeroes(vector<vector<int>> &matrix) {
+    vector<bool> rows(matrix.size(), false);
+    vector<bool> cols(matrix[0].size(), false);
+    for (int i = 0; i < matrix.size(); i++)
+      for (int j = 0; j < matrix[0].size(); j++)
+        if (matrix[i][j] == 0) {
+          rows[i] = true;
+          cols[j] = true;
+        }
+    for (int i = 0; i < matrix.size(); i++)
+      for (int j = 0; j < matrix[0].size(); j++)
+        if (rows[i] || cols[j])
+          matrix[i][j] = 0;
+  }
+};
+
+int main() {
+  vector<vector<int>> matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+  Solution().setZeroes(matrix);
+  cout << matrix;
+}
