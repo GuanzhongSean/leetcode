2e77a8babbc7f853d6a86366519febd883f04f5e
Jiaze Xiao <sean.xiao@uwaterloo.ca>
2024-05-02 18:46:35 -0400

    upload q75

---
+++ q75/solution.cc
@@ -0,0 +1,23 @@
+#include "../include/utils.h"
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  void sortColors(vector<int> &nums) {
+    vector<int> colour(3, 0);
+    for (int n : nums)
+      colour[n]++;
+    int i = 0;
+    for (int c = 0; c < 3; c++)
+      for (int j = 0; j < colour[c]; j++)
+        nums[i++] = c;
+  }
+};
+
+int main() {
+  vector<int> v{2, 0, 2, 1, 1, 0};
+  Solution().sortColors(v);
+  cout << v;
+}
