acce1c43db7efb8cb1c525b011c107a917577b42
Sean Xiao <j76xiao@uwaterloo.ca>
2024-08-20 16:22:33 -0400

    Update include

--- include/utils.cc
+++ include/utils.cc
@@ -1,15 +1,5 @@
 #include "utils.h"
 
-template <typename T>
-void print(vector<T> v) {
-  for (T i : v) {
-    cout << i << " ";
-  }
-  cout << endl;
-}
-
-template <typename T>
-inline ostream &operator<<(ostream &os, vector<T> v) {
-  for (T i : v) os << i << " ";
-  return os << endl;
+inline void print(ListNode *head) {
+  if (head) head->print();
 }
--- include/utils.h
+++ include/utils.h
@@ -4,9 +4,17 @@
 #include "ListNode.h"
 
 template <typename T>
-void print(vector<T> v);
+void print(vector<T> v) {
+  for (T i : v) cout << i << " ";
+  cout << endl;
+}
+
+void print(ListNode *head);
 
 template <typename T>
-inline ostream &operator<<(ostream &os, vector<T> v);
+inline ostream &operator<<(ostream &os, vector<T> v) {
+  for (T i : v) os << i << " ";
+  return os << endl;
+}
 
 #endif
