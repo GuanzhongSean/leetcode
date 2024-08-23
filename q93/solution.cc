b67df9471a5b04e67e4cbff1608695d5fb5d32f3
Sean Xiao <j76xiao@uwaterloo.ca>
2024-08-23 18:29:20 -0400

    Update q93

--- q93/solution.cc
+++ q93/solution.cc
@@ -4,7 +4,7 @@ namespace q93 {
 
 class Solution {
  private:
-  void dfs(const string& s, int start, int step, vector<string>& path,
+  void backtrack(const string& s, int start, int step, vector<string>& path,
            vector<string>& result) {
     if (start == s.size() && step == 4) {
       result.push_back(path[0] + "." + path[1] + "." + path[2] + "." + path[3]);
@@ -17,7 +17,7 @@ class Solution {
       num = num * 10 + (s[i] - '0');
       if (num <= 255) {
         path.push_back(s.substr(start, i - start + 1));
-        dfs(s, i + 1, step + 1, path, result);
+        backtrack(s, i + 1, step + 1, path, result);
         path.pop_back();
       }
       if (num == 0) break;
@@ -28,7 +28,7 @@ class Solution {
   vector<string> restoreIpAddresses(string s) {
     vector<string> result;
     vector<string> path;
-    dfs(s, 0, 0, path, result);
+    backtrack(s, 0, 0, path, result);
     return result;
   }
 };
