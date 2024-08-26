01ea478ce953257826707c4ea04e9a6a12dddc95
Sean Xiao <j76xiao@uwaterloo.ca>
2024-08-26 15:35:32 -0400

    Upload q101

---
+++ q101/solution.cc
@@ -0,0 +1,27 @@
+#include "utils.h"
+
+namespace q100 {
+
+class Solution {
+  bool isSymmetric(TreeNode* left, TreeNode* right) {
+    if (!left && !right) return true;
+    if (!left || !right) return false;
+    return left->val == right->val && isSymmetric(left->left, right->right) &&
+           isSymmetric(left->right, right->left);
+  }
+
+ public:
+  bool isSymmetric(TreeNode* root) {
+    return !root || isSymmetric(root->left, root->right);
+  }
+};
+
+}  // namespace q100
+
+int main() {
+  TreeNode* root = new TreeNode({1, 2, 2, 3, 4, 4, 3});
+  print(root);
+  auto result = q100::Solution().isSymmetric(root);
+  print(result);
+  delete root;
+}
