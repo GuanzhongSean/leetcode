f7a28a1abc7f28f47dd51e4ed29f25bc1611e906
Sean Xiao <j76xiao@uwaterloo.ca>
2024-12-20 15:13:49 -0500

    Update include/utils

--- include/utils.cc
+++ include/utils.cc
@@ -2,13 +2,13 @@
 
 namespace {
 
-static char init = []() -> char {
+static const char _([]() -> char {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   cout.tie(nullptr);
   cout << boolalpha;
   print("Loaded include/utils.");
   return '$';
-}();
+}());
 
 }  // namespace
