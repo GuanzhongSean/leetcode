6d260a14f4aae4c470a1fc090c7fc25d4de6f091
Sean Xiao <j76xiao@uwaterloo.ca>
2024-12-27 17:12:44 -0500

    Format .js files

--- q125/solution.js
+++ q125/solution.js
@@ -2,28 +2,22 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var assert = require("assert");
 function isPalindrome(s) {
-    s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
-    var i = 0, j = s.length - 1;
-    while (i < j) {
-        if (s[i] !== s[j])
-            return false;
-        i++;
-        j--;
-    }
-    return true;
+  s = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  var i = 0,
+    j = s.length - 1;
+  while (i < j) {
+    if (s[i] !== s[j]) return false;
+    i++;
+    j--;
+  }
+  return true;
 }
-;
 // Test cases
-var true_test_cases = [
-    "A man, a plan, a canal: Panama",
-    " "
-];
-var false_test_cases = [
-    "race a car"
-];
+var true_test_cases = ["A man, a plan, a canal: Panama", " "];
+var false_test_cases = ["race a car"];
 true_test_cases.forEach(function (test_case) {
-    assert.strictEqual(isPalindrome(test_case), true);
+  assert.strictEqual(isPalindrome(test_case), true);
 });
 false_test_cases.forEach(function (test_case) {
-    assert.strictEqual(isPalindrome(test_case), false);
+  assert.strictEqual(isPalindrome(test_case), false);
 });
--- q128/solution.js
+++ q128/solution.js
@@ -1,47 +1,59 @@
 "use strict";
-var __values = (this && this.__values) || function(o) {
-    var s = typeof Symbol === "function" && Symbol.iterator, m = s && o[s], i = 0;
+var __values =
+  (this && this.__values) ||
+  function (o) {
+    var s = typeof Symbol === "function" && Symbol.iterator,
+      m = s && o[s],
+      i = 0;
     if (m) return m.call(o);
-    if (o && typeof o.length === "number") return {
+    if (o && typeof o.length === "number")
+      return {
         next: function () {
-            if (o && i >= o.length) o = void 0;
-            return { value: o && o[i++], done: !o };
-        }
-    };
-    throw new TypeError(s ? "Object is not iterable." : "Symbol.iterator is not defined.");
-};
+          if (o && i >= o.length) o = void 0;
+          return { value: o && o[i++], done: !o };
+        },
+      };
+    throw new TypeError(
+      s ? "Object is not iterable." : "Symbol.iterator is not defined.",
+    );
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 var assert = require("assert");
 function longestConsecutive(nums) {
-    var e_1, _a;
-    if (nums.length === 0)
-        return 0;
-    var numSet = new Set(nums);
-    var longestStreak = 0;
+  var e_1, _a;
+  if (nums.length === 0) return 0;
+  var numSet = new Set(nums);
+  var longestStreak = 0;
+  try {
+    for (
+      var numSet_1 = __values(numSet), numSet_1_1 = numSet_1.next();
+      !numSet_1_1.done;
+      numSet_1_1 = numSet_1.next()
+    ) {
+      var num = numSet_1_1.value;
+      // Only start a new sequence if the number is the start of a sequence
+      if (!numSet.has(num - 1)) {
+        var currentNum = num;
+        var currentStreak = 1;
+        // Check for consecutive numbers
+        while (numSet.has(currentNum + 1)) {
+          currentNum += 1;
+          currentStreak += 1;
+        }
+        longestStreak = Math.max(longestStreak, currentStreak);
+      }
+    }
+  } catch (e_1_1) {
+    e_1 = { error: e_1_1 };
+  } finally {
     try {
-        for (var numSet_1 = __values(numSet), numSet_1_1 = numSet_1.next(); !numSet_1_1.done; numSet_1_1 = numSet_1.next()) {
-            var num = numSet_1_1.value;
-            // Only start a new sequence if the number is the start of a sequence
-            if (!numSet.has(num - 1)) {
-                var currentNum = num;
-                var currentStreak = 1;
-                // Check for consecutive numbers
-                while (numSet.has(currentNum + 1)) {
-                    currentNum += 1;
-                    currentStreak += 1;
-                }
-                longestStreak = Math.max(longestStreak, currentStreak);
-            }
-        }
+      if (numSet_1_1 && !numSet_1_1.done && (_a = numSet_1.return))
+        _a.call(numSet_1);
+    } finally {
+      if (e_1) throw e_1.error;
     }
-    catch (e_1_1) { e_1 = { error: e_1_1 }; }
-    finally {
-        try {
-            if (numSet_1_1 && !numSet_1_1.done && (_a = numSet_1.return)) _a.call(numSet_1);
-        }
-        finally { if (e_1) throw e_1.error; }
-    }
-    return longestStreak;
+  }
+  return longestStreak;
 }
 // Example usage:
 var nums = [100, 4, 200, 1, 3, 2];
