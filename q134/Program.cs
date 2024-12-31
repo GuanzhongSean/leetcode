748e9eb491e9d45d63b2c06bd9d4c742f79cc9b9
Sean Xiao <j76xiao@uwaterloo.ca>
2024-12-30 20:47:22 -0500

    Upload q134

---
+++ q134/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace q134
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Manual testing of Solution
+            var solution = new Solution();
+            int[] gas = { 1, 2, 3, 4, 5 };
+            int[] cost = { 3, 4, 5, 1, 2 };
+            Console.WriteLine(solution.CanCompleteCircuit(gas, cost)); // Expected: 3
+
+            int[] gas2 = { 2, 3, 4 };
+            int[] cost2 = { 3, 4, 3 };
+            Console.WriteLine(solution.CanCompleteCircuit(gas2, cost2)); // Expected: -1
+        }
+    }
+}
---
+++ q134/src/Solution.cs
@@ -0,0 +1,25 @@
+namespace q134
+{
+    public class Solution
+    {
+        public int CanCompleteCircuit(int[] gas, int[] cost)
+        {
+            int totalTank = 0, currTank = 0, startingStation = 0;
+
+            for (int i = 0; i < gas.Length; i++)
+            {
+                totalTank += gas[i] - cost[i];
+                currTank += gas[i] - cost[i];
+
+                // If we can't reach the next station
+                if (currTank < 0)
+                {
+                    startingStation = i + 1;
+                    currTank = 0;
+                }
+            }
+
+            return totalTank >= 0 ? startingStation : -1;
+        }
+    }
+}
