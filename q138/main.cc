a772e6de9308a97d0d1f66d0ea4c94ab2bf1995e
Sean Xiao <j76xiao@uwaterloo.ca>
2025-01-03 20:48:59 -0500

    Upload q138

---
+++ q138/main.cc
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+
+#include "Node.h"
+#include "Utils.h"
+#include "solution.cc"
+
+constexpr auto null = Random::Node::null;
+
+int main() {
+    Random::Node* head = new Random::Node({{7, null}, {13, 0}, {11, 4}, {10, 2}, {1, 0}});
+    print(head);
+    auto result = q138::Solution().copyRandomList(head);
+    print("Result:", result);
+    EXPECT_EQ(result->val, 7);
+    EXPECT_EQ(result->random, nullptr);
+    EXPECT_EQ(result->next->val, 13);
+    EXPECT_EQ(result->next->random->val, 7);
+    EXPECT_EQ(result->next->random, result);
+    EXPECT_EQ(result->next->random->next->next->val, 11);
+    print_human_readable_typename<decltype(result)>();
+}
---
+++ q138/solution.cc
@@ -0,0 +1,33 @@
+#include <unordered_map>
+
+#include "Node.h"
+
+namespace q138 {
+
+using Node = Random::Node;
+
+class Solution {
+   public:
+    Node* copyRandomList(Node* head) {
+        if (!head) return nullptr;
+
+        std::unordered_map<Node*, Node*> old_to_new;
+
+        Node* curr = head;
+        while (curr) {
+            old_to_new[curr] = new Node(curr->val);
+            curr = curr->next;
+        }
+
+        curr = head;
+        while (curr) {
+            old_to_new[curr]->next = old_to_new[curr->next];
+            old_to_new[curr]->random = old_to_new[curr->random];
+            curr = curr->next;
+        }
+
+        return old_to_new[head];
+    }
+};
+
+}  // namespace q138
