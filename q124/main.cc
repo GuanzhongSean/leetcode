0be8b6d436d3bf6252399792eebf95265b9b8c6a
Sean Xiao <j76xiao@uwaterloo.ca>
2025-11-30 21:01:03 -0500

    Update q124

--- q124/main.cc
+++ q124/main.cc
@@ -1,12 +1,11 @@
-#include <gtest/gtest.h>
-
 #include "solution.cc"
 
 constexpr auto null = TreeNode::null;
 
 int main() {
-	TreeNode* root = new TreeNode(-3);
+	TreeNode* root = new TreeNode({-10,9,20,null,null,15,7});
+	print(root);
 	auto result = q124::Solution().maxPathSum(root);
 	print(result);
-	EXPECT_EQ(result, -3);
+	EXPECT_EQ(result, 42);
 }
--- q124/solution.cc
+++ q124/solution.cc
@@ -1,22 +1,18 @@
-#include <algorithm>
-
-#include "TreeNode.h"
 #include "Utils.h"
 
 namespace q124 {
 
 using namespace std;
 
+namespace V1 {
 class Solution {
 	pair<int, int> dfs(TreeNode* root) {
-		if (!root) {
-			return {0, INT_MIN};
-		}
+		if (!root) return {0, INT_MIN};
 		auto [leftMax, leftMaxPath] = dfs(root->left);
 		auto [rightMax, rightMaxPath] = dfs(root->right);
 
 		int maxPathThroughRoot = leftMax + rightMax + root->val;
-		int maxPath = max({leftMaxPath, rightMaxPath, maxPathThroughRoot, root->val});
+		int maxPath = max({leftMaxPath, rightMaxPath, maxPathThroughRoot});
 		int maxSum = max(0, max(leftMax, rightMax) + root->val);
 		return {maxSum, maxPath};
 	}
@@ -24,5 +20,27 @@ class Solution {
    public:
 	int maxPathSum(TreeNode* root) { return dfs(root).second; }
 };
+}  // namespace V1
+
+inline namespace V2 {
+class Solution {
+	int maxPath = INT_MIN;
+	int dfs(TreeNode* root) {
+		if (!root) return 0;
+		int leftMax = dfs(root->left);
+		int rightMax = dfs(root->right);
+
+		int maxPathThroughRoot = leftMax + rightMax + root->val;
+		maxPath = max(maxPath, maxPathThroughRoot);
+		return max(0, max(leftMax, rightMax) + root->val);
+	}
+
+   public:
+	int maxPathSum(TreeNode* root) {
+		dfs(root);
+		return maxPath;
+	}
+};
+}  // namespace V2
 
 }  // namespace q124
